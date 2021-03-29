298b3e40b7e718c29f4132c649591888dccfd3ea
Leandro Souza dos Santos <l.souzadsantos@gmail.com>
2021-03-29 20:45:19 -0300

    Adição de testes unitários

---
+++ EventsAPI/Events.Tests/EventsTests.cs
@@ -0,0 +1,66 @@
+using Events.API.Models.Entities;
+using Events.API.Models.ViewModels;
+using Events.API.Services.EventsServices;
+using Events.API.Services.Repositories.EventsRepository;
+using NSubstitute;
+using System.Threading.Tasks;
+using Xunit;
+
+namespace Events.Tests
+{
+    public class Tests
+    {
+        [Fact]
+        public async Task ShouldSaveEventAsProcessed()
+        {
+            Event savedEvent = null;
+            var eventRepository = Substitute.For<IEventRepository>();
+            var eventServices = new EventServices(eventRepository);
+            await eventRepository.AddEventAsync(Arg.Do<Event>(e => savedEvent = e));
+
+            var eventViewModel = new EventViewModel
+            {
+                Tag = "tag",
+                TimeStamp = 15618486,
+                Valor = "valor teste"
+            };
+
+            await eventServices.ProcessEvents(eventViewModel);
+
+            Received.InOrder(async () =>
+            {
+                await eventRepository.AddEventAsync(Arg.Any<Event>());
+                await eventRepository.SaveAsync();
+            });
+            Assert.NotNull(savedEvent);
+            Assert.True(savedEvent.Processed);
+        }
+
+        [Fact]
+        public async Task ShouldNotSaveEventAsProcessed()
+        {
+            Event savedEvent = null;
+            var eventRepository = Substitute.For<IEventRepository>();
+            var eventServices = new EventServices(eventRepository);
+
+            await eventRepository.AddEventAsync(Arg.Do<Event>(e => savedEvent = e));
+
+            var eventViewModel = new EventViewModel
+            {
+                Tag = "tag",
+                TimeStamp = 15618486,
+                Valor = null
+            };
+
+            await eventServices.ProcessEvents(eventViewModel);
+
+            Received.InOrder(async () =>
+            {
+                await eventRepository.AddEventAsync(Arg.Any<Event>());
+                await eventRepository.SaveAsync();
+            });
+            Assert.NotNull(savedEvent);
+            Assert.False(savedEvent.Processed);
+        }
+    }
+}
